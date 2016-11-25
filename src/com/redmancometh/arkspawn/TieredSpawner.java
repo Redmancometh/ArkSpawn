e9b70c98194ef3b406b52a24447cf5872f41c574
Redmancometh <Brendanstcurry@gmail.com>
2016-11-24 21:16:39 -0600

    Add the actual spawning

--- src/com/redmancometh/arkspawn/TieredSpawner.java
+++ src/com/redmancometh/arkspawn/TieredSpawner.java
@@ -1,6 +1,7 @@
 package com.redmancometh.arkspawn;
 
 import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
 
 import com.redmancometh.arkspawn.config.SpawnerTier;
 
@@ -8,15 +9,17 @@ public class TieredSpawner
 {
     private Location location;
     private SpawnerTier tier;
-    
-    public TieredSpawner(Location location, SpawnerTier tier)
+    private EntityType type; //so that we don't need to call getBlock and fuck with spawner data.
+
+    public TieredSpawner(Location location, SpawnerTier tier, EntityType type)
     {
         this.location = location;
+        this.type = type;
+        this.location = location;
     }
-    
+
     public void tick()
     {
-        
     }
 
     public Location getLocation()
